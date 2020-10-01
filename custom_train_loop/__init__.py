40a69f538dcbddbe28aa6b6bfe5ee0a14a3bdd38
1chimaruGin <kyitharhein18@gmail.com>
2020-10-01 15:01:22 +0630

    UPload

---
+++ custom_train_loop/__init__.py
@@ -0,0 +1,1 @@
+from custom_train_loop import custom_training_loop
--- setup.py
+++ setup.py
@@ -1,27 +1,44 @@
-from distutils.core import setup
+import pip
+import logging
+import pkg_resources
 
+try:
+    from setuptools import setup
+except ImportError:
+    from distutils.core import setup
+
+
+def _parse_requirements(filename):
+    """ load requirements from a pip requirements file """
+    lineiter = (line.strip() for line in open(filename))
+    return [line for line in lineiter if line and not line.startswith("#")]
+
+
+# parse_requirements() returns generator of pip.req.InstallRequirement objects
+try:
+    install_reqs = _parse_requirements("requirements.txt")
+except Exception:
+    logging.warning("Fail load requirements file, so using default ones.")
+    install_reqs = []
 
 setup(
-  name = 'ctl',         # How you named your package folder (MyLib)
-  packages = ['ctl'],   # Chose the same as "name"
-  version = '0.1',      # Start with a small number and increase it with every change you make
-  license='MIT',        # Chose a license from here: https://help.github.com/articles/licensing-a-repository
-  description = 'TESTING FIRST PYPI PACKAGE',   # Give a short description about your library
-  author = 'Kyi Thar Hein',                   # Type in your name
-  author_email = 'kyitharhein18@gmail.com',      # Type in your E-Mail
-  url = 'https://github.com/user/reponame',   # Provide either the link to your github or to your website
-  download_url = 'https://github.com/user/reponame/archive/v_01.tar.gz',    # I explain this later on
-  keywords = ['SOME', 'MEANINGFULL', 'KEYWORDS'],   # Keywords that define your package best
-  install_requires=[            # I get to this in a second
-          'validators',
-          'beautifulsoup4',
-      ],
-  classifiers=[
-    'Development Status :: 3 - Alpha',      # Chose either "3 - Alpha", "4 - Beta" or "5 - Production/Stable" as the current state of your package
-    'Intended Audience :: Developers',      # Define that your audience are developers
+    name="custom_train_loop",
+    packages=["custom_train_loop"],
+    version="0.1",
+    license="MIT",
+    description="TESTING FIRST PYPI PACKAGE",
+    author="Kyi Thar Hein",
+    author_email="kyitharhein18@gmail.com",
+    url="https://github.com/1chimaruGin/custom_train_loop",
+    download_url="https://github.com/1chimaruGin/custom_train_loop/archive/v_01.tar.gz",
+    keywords=["CUSTOM", "TRAINING", "LOOP"],
+    install_requires=install_reqs,
+    classifiers=[
+    'Development Status :: 3 - Alpha',
+    'Intended Audience :: Developers',
     'Topic :: Software Development :: Build Tools',
-    'License :: OSI Approved :: MIT License',   # Again, pick a license
-    'Programming Language :: Python :: 3',      #Specify which pyhton versions that you want to support
+    'License :: OSI Approved :: MIT License',
+    'Programming Language :: Python :: 3',
     'Programming Language :: Python :: 3.4',
     'Programming Language :: Python :: 3.5',
     'Programming Language :: Python :: 3.6',
